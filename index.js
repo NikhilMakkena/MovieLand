3dfa37a4152fd988952860f923aec93a8b3b6bc5
NikhilMakkena <43212782+NikhilMakkena@users.noreply.github.com>
2023-04-10 17:32:17 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import * as ReactDOMClient from 'react-dom/client';
+
+import App from "./App";
+
+// ReactDOMClient.createRoot(document.getElementById('app')).render(<App tab="home" />);
+ReactDOM.render(<App/>, document.getElementById('root'));
